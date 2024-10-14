a5bb4f33189418803472baf70a5f666e07ab6bf0
LarissaGu <68541012+LarissaGu@users.noreply.github.com>
2024-10-14 22:19:55 +0200

    Added Search engine

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Dictionary </h1>
+        <h1>Dictionary</h1>
+
+        <Dictionary />
       </header>
     </div>
   );
---
+++ src/Dictionary.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import "./Dictionary.css";
+
+export default function Dictionary() {
+  let [keyword, setKeyword] = useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    alert(`Searching for ${keyword}`);
+  }
+
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
+
+  return (
+    <div className="Dictionary">
+      <form onSubmit={handleSubmit}>
+        <input
+          type="search"
+          placeholder="Search for something"
+          onChange={handleKeywordChange}
+        />
+        <input type="submit" value="Search" className="btn btn-primary" />
+      </form>
+    </div>
+  );
+}
