79eda99875ee99a131a2203438dd5ff5191b19a0
LarissaGu <68541012+LarissaGu@users.noreply.github.com>
2024-10-15 19:19:16 +0200

    Added the Meaning component
    The Meaning component displays the partOfSpeech, definition and example of the searched word

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,17 +1,18 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Results from "./Results";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
 
   function handleResponse(response) {
-    console.log(response);
+    setResults(response.data);
   }
 
   function handleSubmit(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword}`);
 
     let apiKey = "f1adaca67t6e03d488b4ca1d5710830o";
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
@@ -32,6 +33,7 @@ export default function Dictionary() {
         />
         <input type="submit" value="Search" className="btn btn-primary" />
       </form>
+      <Results results={results} />
     </div>
   );
 }
